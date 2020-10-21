b2d384ea1efe2f4fb01b9fd8cd9f2edcf5b8a893
Maki Miyagawa <mkmy1123@gmail.com>
2020-10-21 17:11:12 +0900

    finish active_storage

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -39,17 +39,17 @@ class TasksController < ApplicationController
 
   def update
     @task.update!(task_params)
-    redirect_to tasks_url, notice: "タスク「#{task.name}」を更新しました。"
+    redirect_to tasks_url, notice: "タスク「#{@task.name}」を更新しました。"
   end
 
   def destroy
     @task.destroy!
-    redirect_to tasks_url, notice: "タスク「#{task.name}」を削除しました。"
+    redirect_to tasks_url, notice: "タスク「#{@task.name}」を削除しました。"
   end
 
   private
     def task_params
-      params.require(:task).permit(:name, :description)
+      params.require(:task).permit(:name, :description, :image)
     end
 
     def set_task
